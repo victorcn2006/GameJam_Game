a31f05a8ec5a54781d70f9d2e4c2c74182535428
Alan Rodríguez <81470568+AlanRC9@users.noreply.github.com>
2026-05-16 01:15:44 +0200

    Puzzle2 Base Finished

---
+++ Assets/Code/Scripts/Puzzles/Puzzle2/Deflector.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class Deflector : MonoBehaviour, IInteractive
+{
+    [SerializeField] private int initialPuzzlePositionId;
+    [SerializeField] private PuzzlePositions _puzzlePositions;
+    [SerializeField] private float moveDuration = 0.3f;
+    [SerializeField] private GameObject _deflector;
+
+    private int _currentRowId;
+    private int _currentColumnId;
+
+    private DeflectorSide.Side _pendingSide;
+
+
+    private void Start()
+    {
+        _currentRowId = initialPuzzlePositionId / 3;
+        _currentColumnId = initialPuzzlePositionId % 3;
+        transform.position = _puzzlePositions.GetPosition(_currentRowId, _currentColumnId).position;
+    }
+
+    public void Interact()
+    {
+
+        switch (_pendingSide)
+        {
+            case DeflectorSide.Side.Left:
+                _currentColumnId = Mathf.Clamp(_currentColumnId + 1, 0, 2);
+                break;
+            case DeflectorSide.Side.Right:
+                _currentColumnId = Mathf.Clamp(_currentColumnId - 1, 0, 2);
+                break;
+            case DeflectorSide.Side.Front:
+                _currentRowId = Mathf.Clamp(_currentRowId + 1, 0, 2);
+                break;
+            case DeflectorSide.Side.Back:
+                _currentRowId = Mathf.Clamp(_currentRowId - 1, 0, 2);
+                break;
+        }
+
+        Transform target = _puzzlePositions.GetPosition(_currentRowId, _currentColumnId);
+        transform.DOMove(target.position, moveDuration);
+    }
+
+    public void OnSidePlayerDetected(DeflectorSide.Side side)
+    {
+        _pendingSide = side;
+    }
+}
