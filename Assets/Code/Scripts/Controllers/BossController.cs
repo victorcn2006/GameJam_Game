d02a90dc9c9d7e2011ff658c1dbc4fc2ab71a36d
GuillemDehonFuentes <156358165+GuillemDehonFuentes@users.noreply.github.com>
2026-05-15 20:40:32 +0200

    Subir cosas modelo

--- Assets/Code/Scripts/Controllers/BossController.cs
+++ Assets/Code/Scripts/Controllers/BossController.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using StateMachine.Runtime;
 
 public class BossController : MonoBehaviour
 {
+    StateMachineComponent sm;
+    public Context bossContext;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        sm = GetComponent<StateMachineComponent>();
+        bossContext = sm.GetStateContext();
     }
 
     // Update is called once per frame
