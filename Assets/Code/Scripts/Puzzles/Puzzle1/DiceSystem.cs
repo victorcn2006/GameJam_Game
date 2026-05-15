896ff94ad8a51c22647790e502daba8538772f00
Alan Rodríguez <81470568+AlanRC9@users.noreply.github.com>
2026-05-15 20:11:18 +0200

    PrimerPuzzleBase

---
+++ Assets/Code/Scripts/Puzzles/Puzzle1/DiceSystem.cs
@@ -0,0 +1,35 @@
+using DG.Tweening;
+using System;
+using UnityEngine;
+using UnityEngine.AdaptivePerformance;
+
+public class DiceSystem : MonoBehaviour
+{
+
+    [SerializeField] private GameObject dice;
+    [SerializeField] private int diceID;
+    private bool _resolved = false;
+    private PuzzleOneManager _puzzleOneManager;
+
+    private void Awake()
+    {
+        _puzzleOneManager = GetComponentInParent<PuzzleOneManager>();
+    }
+
+    void Start()
+    {
+        dice.transform
+            .DOLocalRotate(new Vector3(0f, 0f, -450f), 1.2f, RotateMode.FastBeyond360)
+            .SetRelative()
+            .SetEase(Ease.InQuad)
+            .OnComplete(Check);
+    }
+
+    public void SetDiceState(bool state) => _resolved = state;
+
+    private void Check()
+    {
+        if (_resolved) _puzzleOneManager.DiceState(diceID, _resolved);
+    }
+
+}
