91c071c0d1360c10347d49995a8b21db3c85a39d
GuillemDehonFuentes <156358165+GuillemDehonFuentes@users.noreply.github.com>
2026-05-15 22:52:34 +0200

    Subir cosas de mometno

--- Assets/Code/Scripts/Controllers/BossController.cs
+++ Assets/Code/Scripts/Controllers/BossController.cs
@@ -1,21 +1,102 @@
 using UnityEngine;
 using StateMachine.Runtime;
+using UnityEngine.Events;
 
 public class BossController : MonoBehaviour
 {
     StateMachineComponent sm;
-    public Context bossContext;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Boss Stats")]
+    [SerializeField] int maxHP = 3;
+    [SerializeField] int currentHP;
+
+    [Header("Boss State")]
+    [SerializeField] bool isDead = false;
+    [SerializeField] bool invulnerable = false;
+
+    [Header("Events")]
+    public UnityEvent OnTakeDamage;
+    public UnityEvent OnDeath;
+    public UnityEvent OnHeal;
+
     void Start()
     {
         sm = GetComponent<StateMachineComponent>();
-        bossContext = sm.GetStateContext();
+
+        currentHP = maxHP;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        string currentState = sm.GetCurrentStateName();
+    }
+
+    // DAMAGE
+
+    public void TakeDamage(int damage)
+    {
+        // Si está muerto o es invulnerable no recibe daño
+        if (isDead || invulnerable)
+            return;
+
+        currentHP -= damage;
+
+        Debug.Log("Boss recibió " + damage + " de daño");
+
+        OnTakeDamage?.Invoke();
+
+        // Evitar vida negativa
+        if (currentHP <= 0)
+        {
+            currentHP = 0;
+            Die();
+        }
+    }
+
+    // DEATH
+
+    void Die()
+    {
+        if (isDead)
+            return;
+
+        isDead = true;
+
+        Debug.Log("Boss derrotado");
+
+        OnDeath?.Invoke();
+    }
+
+    // INVULNERABILITY
+
+    public void SetInvulnerable(bool value)
+    {
+        invulnerable = value;
+    }
+
+    // GETTERS
+
+    public int GetCurrentHP()
+    {
+        return currentHP;
+    }
+
+    public int GetMaxHP()
+    {
+        return maxHP;
+    }
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    // RESET BOSS
+
+    public void ResetBoss()
+    {
+        isDead = false;
+        currentHP = maxHP;
+        invulnerable = false;
     }
 }
